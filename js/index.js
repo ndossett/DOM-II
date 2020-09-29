901ae84c3465049f57baeb1f05c78c0d6f9bcfdd
ndossett <niki.m.dossett@gmail.com>
2020-09-29 17:32:38 -0400

    created 2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+const logoHead = document.querySelector('.logo-heading')
+
+function highlight(event) {
+event.target.style.backgroundColor = 'orange';
+    setTimeout(function() {
+    event.target.style.backgroundColor = '';
+}, 700);  
+}
+logoHead.addEventListener('mouseenter', highlight)
+
+const introimg = document.querySelector('.intro img')
+
+function bigBorder(event) {
+    event.target.style.border = '10px dotted #FF8C00';
+    setTimeout(function() {
+        event.target.style.border = '';
+    }, 500);       
+}
+introimg.addEventListener('mouseover', bigBorder)
